bd6ce56fafd647697de21d46bce9ce1d378ad609
Mormacill <bachfreak15@googlemail.com>
2019-02-02 20:34:15 +0100

    first commit

--- rrf.cpp
+++ rrf.cpp
@@ -7,5 +7,6 @@ return ((v*d)/n);
 
 double vis (double t)
 {
+//* * * Muss noch mit 10^-7 multipliziert werden! * * *
 return -1.16643853*pow(10,-13)*pow(t,5) + 3.60057235*pow(10,-10)*pow(t,4) + -5.304786536*pow(10,-7)*pow(t,3) + 0.001118914391*pow(t,2) + 0.891933997744*t + 135.2163195635;
 }
