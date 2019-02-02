a0a333ed837d8dc4be5ac876f508195d999e211b
Mormacill <bachfreak15@googlemail.com>
2019-02-02 20:59:41 +0100

    first commit

--- main.cpp
+++ main.cpp
@@ -4,7 +4,10 @@ using namespace std;
 
 int main()
 {
+cout << "Rohrreibungsberechnung für ein gerades Rohr" << endl << endl << endl;
 
+cout << "   ┌------------┐" << endl;
+cout << "   └------------┘" << endl;
 
 
 
